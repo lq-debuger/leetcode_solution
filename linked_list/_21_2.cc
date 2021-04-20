509f5b7cccf4bf55f024cb5c391b203a3ddd5235
liqiang <liqiang@lejurobot.com>
2021-04-20 13:57:44 +0800

    Finish 21rd

---
+++ linked_list/_21_2.cc
@@ -0,0 +1,39 @@
+
+
+// leetcode #21 递归的方法
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(NULL) {}
+    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+    {   
+        if (l1==NULL) return l2;
+        if (l2==NULL) return l1;
+        
+        if(l1->val < l2->val)
+        {
+            l1->next = mergeTwoLists(l1->next,l2);
+            return l1;
+        }else
+        {
+            l2->next = mergeTwoLists(l1,l2->next);
+            return l2;
+        }
+    }
+};
+
+
+
